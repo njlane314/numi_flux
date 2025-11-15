7385d779affb7352a3f9e6dd9abb3dcb19ccbb65
Nicholas <107313324+njlane314@users.noreply.github.com>
2025-11-15 15:57:50 +0000

    Update config_numiana.sh

--- config_numiana.sh
+++ config_numiana.sh
@@ -4,7 +4,7 @@ export GCC=v6_4_0
 export ROOT=v6_12_06a
 export BOOST=v1_66_0
 export DK2NU_VER=v01_05_01b
-export PPFX_VER=v02_18_02
+#export PPFX_VER=v02_18_02
 
 export FLUGG_FHC_DIR=
 export FLUGG_RHC_DIR=
