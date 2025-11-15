49e796e97a94fb725473f0fc77ec14968cf7ab94
Nicholas <107313324+njlane314@users.noreply.github.com>
2025-11-15 16:20:44 +0000

    Update config_numiana.sh

--- config_numiana.sh
+++ config_numiana.sh
@@ -6,7 +6,8 @@ export BOOST=v1_66_0
 export DK2NU_VER=v01_05_01b
 #export PPFX_VER=v02_18_02
 
-export FLUGG_FHC_DIR=
-export FLUGG_RHC_DIR=
-export DK2NU_FHC_DIR=
-export DK2NU_RHC_DIR=
+export DK2NU_FHC_DIR=/pnfs/uboone/persistent/users/bnayak/flux_files/uboone_beamsim_g4.10.4_nothresh/me000z200i/run0/files
+export DK2NU_RHC_DIR=/pnfs/uboone/persistent/users/bnayak/flux_files/uboone_beamsim_g4.10.4_nothresh/me000z-200i/run0/files
+
+#export DK2NU_FHC_DIR=
+#export DK2NU_RHC_DIR=
